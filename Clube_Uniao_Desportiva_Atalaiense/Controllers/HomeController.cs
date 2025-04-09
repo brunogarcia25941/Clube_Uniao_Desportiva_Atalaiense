cbf83299ef78649d57a46c8fdc2c50de3c7c1585
brunogarcia25941 <aluno25941@ipt.pt>
2025-04-09 09:48:46 +0100

    Inicio

---
+++ Clube_Uniao_Desportiva_Atalaiense/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Clube_Uniao_Desportiva_Atalaiense.Models;
+
+namespace Clube_Uniao_Desportiva_Atalaiense.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Clube_Uniao_Desportiva_Atalaiense/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clube_Uniao_Desportiva_Atalaiense.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
